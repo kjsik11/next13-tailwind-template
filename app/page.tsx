c2faa7a34950a48f92f31658e76b67e9a13c54af
jongsik <kjsik11@gmail.com>
2023-06-12 17:30:07 +0900

    update tailwind config

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
-import Link from 'next/link';
-
 import { Button } from '@/components';
 
 export default function Home() {
   return (
     <main>
       <p className="text-blue-500">main page</p>
-      <Link href="/hello">Go to Hello</Link>
+
       <Button>button</Button>
     </main>
   );
